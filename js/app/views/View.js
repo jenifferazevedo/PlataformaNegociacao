443ee58bd5b5a1b61c2338c303645b63b9c12b50
Jeniffer Q. F. R. de Azevedo <jeniqf@gmail.com>
2019-10-20 18:35:20 -0300

    //JS: Views

---
+++ js/app/views/View.js
@@ -0,0 +1,11 @@
+class View {
+  constructor(elemento) {
+    this._elemento = elemento;
+  }
+  template() {
+    throw new Error('Um template deve ser implementado!')
+  }
+  update(model) {
+    this._elemento.innerHTML = this.template(model);
+  }
+}
