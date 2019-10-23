4ad254e7cf5769a73fcbcda0fae3d9afe39e4fa8
Jeniffer Q. F. R. de Azevedo <jeniqf@gmail.com>
2019-10-23 15:54:04 -0300

    //JS: Update da Lista automático
    /* Colocado o update para acontecer

--- js/app/models/ListaNegociacoes.js
+++ js/app/models/ListaNegociacoes.js
@@ -1,12 +1,15 @@
 class ListaNegociacoes {
-  constructor() {
+  constructor(update) {
     this._negociacoes = [];
+    this._update = update; 
   }
   adiciona(negociacao) {
     this._negociacoes.push(negociacao);
+    this._update(this);
   }
   esvaziar() {
     this._negociacoes = [];
+    this._update(this);
   }
   get negociacoes() {
     return [].concat(this._negociacoes);
