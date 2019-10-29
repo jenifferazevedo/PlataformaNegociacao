836913fea58436013b7eb21ad904bd71434ac6c5
Jeniffer Q. F. R. de Azevedo <jeniqf@gmail.com>
2019-10-29 14:08:49 -0300

    //Proxy
    /* Limpando a classe, criando proxy, teste1 */

--- js/app/models/ListaNegociacoes.js
+++ js/app/models/ListaNegociacoes.js
@@ -1,15 +1,12 @@
 class ListaNegociacoes {
-  constructor(update) {
+constructor() {
     this._negociacoes = [];
-    this._update = update; 
   }
   adiciona(negociacao) {
     this._negociacoes.push(negociacao);
-    this._update(this);
   }
   esvaziar() {
     this._negociacoes = [];
-    this._update(this);
   }
   get negociacoes() {
     return [].concat(this._negociacoes);
