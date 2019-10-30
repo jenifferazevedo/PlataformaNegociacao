8e62bdcd34fc04a6c490d91aee079549a77440b4
Jeniffer Q. F. R. de Azevedo <jeniqf@gmail.com>
2019-10-30 15:24:44 -0300

    //Request AJAX + NegociacaoService +modificações e Post.html
    /* Foi trabalhado a resquest com o AJAX (sem JQuery) na classe NegociacaoService, algumas modificações para deixar a template na NegociacaoView mais limpa e criada uma pagina para Post que está em desenvolvimento */

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -11,4 +11,7 @@ constructor() {
   get negociacoes() {
     return [].concat(this._negociacoes);
   }
+  get volumeTotal() {
+    return this._negociacoes.reduce((total, n) => total + n.volume, 0.0);
+  }
 }
