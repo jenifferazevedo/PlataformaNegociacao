4aa5d5836a92b6d7431f47e6045632571ede65ce
Jeniffer Q. F. R. de Azevedo <jeniqf@gmail.com>
2019-11-06 14:39:07 -0300

    //Método Post no HttpService e Post.html
    /* Foi melhorado o método 'Post' no HttpService e implementado no Post.html. Falta melhorar o Post.html, introduzindo os outros scripts. */

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -20,6 +20,7 @@ class HttpService {
     return new Promise((resolve, reject) => {
       let xhr = new XMLHttpRequest();
       xhr.open('POST', url, true);
+      xhr.setRequestHeader("Content-Type", "application/json");
       xhr.onreadystatechange = () => {
         if(xhr.readyState == 4) {
           if(xhr.status == 200) {
@@ -28,7 +29,7 @@ class HttpService {
             reject(xhr.responseText);
           }
         }
-      };
+      }
       xhr.send(JSON.stringify(dado));
     });
   }
