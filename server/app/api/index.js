3be29abf693deca61e2f06de3e29d80b678e3885
Jeniffer Q. F. R. de Azevedo <jeniqf@gmail.com>
2019-11-07 19:51:51 -0300

    //Arrumando erro da data
    /* está salvando certo do server mas ao importar, está diminuindo um dia. Note: See the code on NegociacaoService and on the code that import or view of the importation and try to add one day when import */

--- server/app/api/index.js
+++ server/app/api/index.js
@@ -49,8 +49,7 @@ api.listaRetrasada = function(req, res) {
 
 api.cadastraNegociacao = function(req, res) {
 
-   console.log(req.body);
-   req.body.data = new Date(req.body.data.replace(/-/g,'/'));
+   req.body.data = req.body.data.replace(/-/g,'/');
    negociacoes.push(req.body);
    res.status(200).json("Negociação recebida");
 };
